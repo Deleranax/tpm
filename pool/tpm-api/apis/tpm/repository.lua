2cac6b18ab2fa2685e97b447116811528a516421
Alexandre Leconte <aleconte@dwightstudio.fr>
2025-05-18 14:47:43 +0200

    Updated TPM

--- pool/tpm-api/apis/tpm/repository.lua
+++ pool/tpm-api/apis/tpm/repository.lua
@@ -14,7 +14,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-local repository
+local repository = {}
 
 local deptree = require("/apis/deptree")
 local tact = require("/apis/tact")
