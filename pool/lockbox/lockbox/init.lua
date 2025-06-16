25645e5cc8d5970ad6435fafe5ff349c09f7db0c
Alexandre Leconte <aleconte@dwightstudio.fr>
2025-06-16 12:48:15 +0200

    Updated ccpm

--- pool/lockbox/lockbox/init.lua
+++ pool/lockbox/lockbox/init.lua
@@ -1,17 +1,6 @@
 local Lockbox = {};
 
---[[
-package.path =  "./?.lua;"
-                .. "./cipher/?.lua;"
-                .. "./digest/?.lua;"
-                .. "./kdf/?.lua;"
-                .. "./mac/?.lua;"
-                .. "./padding/?.lua;"
-                .. "./test/?.lua;"
-                .. "./util/?.lua;"
-                .. package.path;
---]]
-Lockbox.ALLOW_INSECURE = true;
+Lockbox.ALLOW_INSECURE = false;
 
 Lockbox.insecure = function()
     assert(Lockbox.ALLOW_INSECURE,
